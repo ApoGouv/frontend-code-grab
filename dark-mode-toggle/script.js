1da276b6b20b2746453f12b54019b77d707a2dae
ApostolosG <apostolosg@mentalmedia.gr>
2023-12-14 10:04:55 +0200

    Dark mode toggle snippet

---
+++ dark-mode-toggle/script.js
@@ -0,0 +1,30 @@
+const darkModeToggleHandler = {
+  name: 'SkeletonSnippet handler',
+
+  toggleCheckboxId: 'dark-mode-toggle',
+  toggleCheckboxEl: null,
+
+  currentMethod: null,
+  debugLogIsEnabled: false,
+
+  log: function(message = '', ...args) {
+    if (this.debugLogIsEnabled) {
+      console.log(`${this.name} > [${this.currentMethod}()] ${message}: `, args);
+    }
+  },
+
+  init: function() {
+    this.currentMethod = 'init';
+    this.log('getting started...');
+
+    this.toggleCheckboxEl = document.getElementById(this.toggleCheckboxId);
+
+    this.toggleCheckboxEl.addEventListener('change', (e) => this.darkThemeToggle(e));
+  },
+
+  darkThemeToggle: function(e) {
+    document.body.classList.toggle('dark', e.target.checked);
+  }
+};
+
+darkModeToggleHandler.init();
